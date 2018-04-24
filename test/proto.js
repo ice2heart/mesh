6e1abe761c38207334292cb22f65f19c9a9bff31
Albert Yakupov <albert.yakupov@mapscape.eu>
2018-04-24 13:29:39 +0200

    fix one test

--- test/proto.js
+++ test/proto.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const chai = require('chai');
 const assert = chai.assert;
 const expect = require('chai').expect;
-const testData = fs.readFileSync('mesh.js');
+// const testData = fs.readFileSync('mesh.js');
+const testData = fs.readFileSync('nm.tar.gz');
 
 function shuffle(a) {
   for (let i = a.length; i; i--) {
@@ -55,34 +56,35 @@ describe('Protocol', function () {
       p2.packet(pp);
     }
   });
-  it ('remove packets', function(){
+  it ('remove packets', function(done){
 
     /// ARE BROKEN!
     p2.on('data', (data) => {
-      console.log(Buffer.compare(testData, data));
+      console.log('data',Buffer.compare(testData, data), data.length, testData.length);
       expect(Buffer.compare(testData, data)).to.equal(0);
+      done();
     });
-    /*p.on('packet', (data) => {
-      console.log(data);
-    });*/
+    p2.on('packet', (data) => {
+      console.log('packet',data);
+      p.packet(data);
+    });
     let i = 0;
+    p.on('packet', (data) => {
+      i = i + 1;
+      if (i != 2){
+        p2.packet(data);
+      }
+      //console.log('packet',data);
+    });
+    
     p.rawData(testData);
     var unsort = [];
     for (let pp of p.packets) {
-      console.log(pp);
+      // console.log(pp);
       unsort.push(pp.buff);
     }
     p2.packet(unsort.shift());
     shuffle(unsort);
 
-    for (let pp of unsort) {
-      i = i + 1;
-      if (i != 2){
-        console.log(i);
-
-        p2.packet(pp);
-      }
-      
-    }
   });
 });
